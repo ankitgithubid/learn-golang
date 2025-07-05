3eeb94a9f82234e9282f1376de1d26e7c1c1c574
ankitgithubid <ankitprasad169@gmail.com>
2025-07-05 23:22:56 +0530

    Web Service Gin

--- Reflection_CustomStructTags/main.go
+++ Reflection_CustomStructTags/main.go
@@ -10,18 +10,39 @@ type User struct {
 	Email string `validate:"required,email"`
 }
 
+func validate(val interface{}) error {
+	v := reflect.ValueOf(val)
+	t := reflect.TypeOf(val)
+
+	if v.Kind() == reflect.Struct {
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			fieldType := t.Field(i)
+			tag := fieldType.Tag.Get("validate")
+
+			fmt.Printf("Field %d: %s = %v, tag = %s\n", i, fieldType.Name, fieldValue, tag)
+		}
+	} else {
+		return fmt.Errorf("expected struct, got %s", v.Kind())
+	}
+
+	return nil
+}
+
 func main() {
 	user := User{
 		Name:  "John Doe",
 		Email: "john@test.com",
 	}
 
-	t := reflect.TypeOf(user)
-	fmt.Printf("Name : %s Kind of user = %s\n", t.Name(), t.Kind())
+	fmt.Printf("Validate = %v\n", validate(user))
 
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		tag := field.Tag.Get("validate")
-		fmt.Printf("Field %s, tag = %s\n", field.Name, tag)
-	}
+	//t := reflect.TypeOf(user)
+	//fmt.Printf("Name : %s Kind of user = %s\n", t.Name(), t.Kind())
+	//
+	//for i := 0; i < t.NumField(); i++ {
+	//	field := t.Field(i)
+	//	tag := field.Tag.Get("validate")
+	//	fmt.Printf("Field %s, tag = %s\n", field.Name, tag)
+	//}
 }
